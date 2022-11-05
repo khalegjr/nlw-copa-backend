e8d6d183cf954553c9602f5891083ca21998a90a
Khaled <khalegjr@gmail.com>
2022-11-05 15:24:37 -0300

    Instalação e configuração do pacote cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
 
 const prisma = new PrismaClient({
   log: ['query']
@@ -10,13 +11,18 @@ async function bootstrap(){
     logger: true,
   })
 
+  fastify.register(cors, {
+    origin: true // qualquer aplicação (só no dev)
+  })
+
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
 
     return { count }
   })
 
-  await fastify.listen({port: 3333})
+  await fastify.listen({port: 3333, host: '0.0.0.0'})
+  // host para funcionar na aplicação mobile
 }
 
 bootstrap()
